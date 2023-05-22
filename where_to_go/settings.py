9b46ae2795860ca68e11ea2c905c72cb1bb01096
Manuilov Daniil <manuilovdaniil@yandex.ru>
2023-05-22 22:05:15 +0300

    Hide sensitive variables

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -9,8 +9,13 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
+
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+^e8doy!)l5xb=(e_gz9c3m@$2bmqq1t74p7u%4mz9+0x9pbm@'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', False)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', False)
 
 ALLOWED_HOSTS = []
 
