35043d804048bdef9ad540adbf1ddc18585f84e5
Manuilov Daniil <manuilovdaniil@yandex.ru>
2023-05-27 19:25:57 +0300

    Set up json_url parser

--- places/management/commands/load_place.py
+++ places/management/commands/load_place.py
@@ -12,10 +12,13 @@ class Command(BaseCommand):
     help = 'Создать метку на карте из JSON файла'
 
     def add_arguments(self, parser):
-        parser.add_argument('json_url', nargs=1, type=str)
+        parser.add_argument(
+            'json_url',
+            help='адрес, по которому лежит json-файл с данными о новом месте',
+        )
 
     def handle(self, *args, **options):
-        json_url = options['json_url'][0]
+        json_url = options['json_url']
         try:
             response = requests.get(json_url)
             response.raise_for_status()
